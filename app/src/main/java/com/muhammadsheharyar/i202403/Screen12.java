c66ccda36d9ecaf042b413a39e08c66e2327c188
Muhammad Sheharyar <msheharyarqazi@gmail.com>
2023-09-15 18:45:04 +0500

    ui touchup and navigation remanining

---
+++ app/src/main/java/com/muhammadsheharyar/i202403/Screen12.java
@@ -0,0 +1,14 @@
+package com.muhammadsheharyar.i202403;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Screen12 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen12);
+    }
+}
---
+++ app/src/main/java/com/muhammadsheharyar/i202403/Screen4.java
@@ -0,0 +1,18 @@
+package com.muhammadsheharyar.i202403;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity; // If you're using AppCompat activities
+
+public class Screen4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen4);
+
+    }
+}
